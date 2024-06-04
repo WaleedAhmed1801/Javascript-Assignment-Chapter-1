fb972ffbae8cf68c982ed8cd434e266ae525f164
Waleed Ahmed <w.ahmed1801@gmail.com>
2024-06-05 02:28:16 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+
+alert ("Error! please enter a valid password");
+alert ("Welcome to JS Land... \n Happy Coding.");
+alert ("Welcome to JS Land");
+alert ("Happy Coding!.. Prevent this page from creating a additional dioulge box ");
+alert ("Hello... I can run JS through my web browser's console");
